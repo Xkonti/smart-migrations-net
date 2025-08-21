9655bf79224b772f1571448b666c29a117968b40
Xkonti <xkonti@xkonti.tech>
2025-08-21 02:03:06 -0400

    Complete tests of migrations from single version

--- Tests/SmartMigrations.Test/MigrationAttributeSingleFromTests.cs
+++ Tests/SmartMigrations.Test/MigrationAttributeSingleFromTests.cs
@@ -8,23 +8,26 @@ public class MigrationAttributeSingleFromTests
     #region Constructor: MigrationAttribute(int from, int to, bool shouldAvoid = false)
 
     [Theory]
-    [InlineData(0, 1, false, new int[] { 0 }, 1, false, false)]
-    [InlineData(0, 1, true, new int[] { 0 }, 1, false, true)]
-    [InlineData(8, 31, false, new int[] { 8 }, 31, false, false)]
-    [InlineData(8, 31, true, new int[] { 8 }, 31, false, true)]
-    [InlineData(168, 50, false, new int[] { 168 }, 50, false, false)]
-    [InlineData(168, 50, true, new int[] { 168 }, 50, false, true)]
-    [InlineData(0, int.MaxValue, false, new int[] { 0 }, int.MaxValue, false, false)]
-    [InlineData(0, int.MaxValue, true, new int[] { 0 }, int.MaxValue, false, true)]
-    public void Constructor_DefaultSchema_SingleFrom_Success(int from, int to, bool shouldAvoid, int[] expectedFromVersions, int expectedTo, bool expectedIsRange, bool expectedShouldAvoid)
+    [InlineData(0, 1, false, new[] { 0 })]
+    [InlineData(5, 1, true, new[] { 5 })]
+    [InlineData(8, 31, null, new[] { 8 })]
+    [InlineData(8, -105631, true, new[] { 8 })]
+    [InlineData(168, -50, false, new[] { 168 })]
+    [InlineData(-168, 50, true, new[] { -168 })]
+    [InlineData(0, int.MaxValue, false, new[] { 0 })]
+    [InlineData(int.MaxValue, int.MinValue, true, new[] { int.MaxValue })]
+    public void Constructor_DefaultSchema_SingleFrom_Success(int from, int to, bool? shouldAvoid, int[] expectedFromVersions)
     {
-        var attribute = new MigrationAttribute(from, to, shouldAvoid);
-        Assert.Equal(expectedFromVersions, attribute.FromVersions);
-        Assert.Equal(expectedTo, attribute.ToVersion);
-        Assert.Equal(expectedIsRange, attribute.IsRange);
-        Assert.Equal(expectedShouldAvoid, attribute.ShouldAvoid);
+        var attribute = shouldAvoid.HasValue
+            ? new MigrationAttribute(from, to, shouldAvoid.Value)
+            : new MigrationAttribute(from, to);
         Assert.Null(attribute.FromSchema);
+        Assert.Equal(expectedFromVersions, attribute.FromVersions);
+        Assert.False(attribute.IsRange);
         Assert.Null(attribute.ToSchema);
+        Assert.Equal(to, attribute.ToVersion);
+        if (shouldAvoid.HasValue) Assert.Equal(shouldAvoid.Value, attribute.ShouldAvoid);
+        else Assert.False(attribute.ShouldAvoid);
     }
 
     [Theory]
@@ -33,6 +36,7 @@ public class MigrationAttributeSingleFromTests
     [InlineData(-42, -42, true)]
     [InlineData(100, 100, false)]
     [InlineData(int.MaxValue, int.MaxValue, true)]
+    [InlineData(int.MinValue, int.MinValue, true)]
     public void Constructor_DefaultSchema_SingleFrom_SameFromTo_ThrowsException(int from, int to, bool shouldAvoid)
     {
         Assert.Throws<ArgumentException>(() => new MigrationAttribute(from, to, shouldAvoid));
@@ -43,33 +47,34 @@ public class MigrationAttributeSingleFromTests
     #region Constructor: MigrationAttribute(string? schema, int from, int to, bool shouldAvoid = false)
 
     [Theory]
-    [InlineData(null, 0, 1, false, new int[] { 0 }, 1, false, false)]
-    [InlineData(null, 0, 1, true, new int[] { 0 }, 1, false, true)]
-    [InlineData("free", 1, 2, false, new int[] { 1 }, 2, false, false)]
-    [InlineData("free", 1, 2, true, new int[] { 1 }, 2, false, true)]
-    [InlineData("paid", 10, 20, false, new int[] { 10 }, 20, false, false)]
-    [InlineData("paid", 10, 20, true, new int[] { 10 }, 20, false, true)]
-    [InlineData("enterprise", 100, 50, false, new int[] { 100 }, 50, false, false)]
-    [InlineData("enterprise", 100, 50, true, new int[] { 100 }, 50, false, true)]
-    public void Constructor_SpecificSchema_SingleFrom_Success(string? schema, int from, int to, bool shouldAvoid, int[] expectedFromVersions, int expectedTo, bool expectedIsRange, bool expectedShouldAvoid)
+    [InlineData(null, 0, 1, false, new[] { 0 }, null)]
+    [InlineData(null, 0, -1, true, new[] { 0 }, null)]
+    [InlineData("free", 1, 2, false, new[] { 1 }, "free")]
+    [InlineData("fr ee", -6, 2, true, new[] { -6 }, "fr ee")]
+    [InlineData("paid ", 10, 20, false, new[] { 10 }, "paid")]
+    [InlineData("  \npaid", int.MaxValue, 20, true, new[] { int.MaxValue }, "paid")]
+    [InlineData("enterprise", 100, 50, false, new[] { 100 }, "enterprise")]
+    [InlineData("\n enterprise \t\t", int.MinValue, int.MinValue + 1, true, new[] { int.MinValue }, "enterprise")]
+    [InlineData(" 123 How Are You?\n\r", 100, -50, true, new[] { 100 }, "123 How Are You?")]
+    public void Constructor_SpecificSchema_SingleFrom_Success(string? schema, int from, int to, bool shouldAvoid, int[] expectedFromVersions, string? expectedSchema)
     {
         var attribute = new MigrationAttribute(schema, from, to, shouldAvoid);
+        Assert.Equal(expectedSchema, attribute.FromSchema);
         Assert.Equal(expectedFromVersions, attribute.FromVersions);
-        Assert.Equal(expectedTo, attribute.ToVersion);
-        Assert.Equal(expectedIsRange, attribute.IsRange);
-        Assert.Equal(expectedShouldAvoid, attribute.ShouldAvoid);
-        Assert.Equal(schema, attribute.FromSchema);
-        Assert.Equal(schema, attribute.ToSchema);
+        Assert.False(attribute.IsRange);
+        Assert.Equal(expectedSchema, attribute.ToSchema);
+        Assert.Equal(to, attribute.ToVersion);
+        Assert.Equal(shouldAvoid, attribute.ShouldAvoid);
     }
 
     [Theory]
-    [InlineData("", 1, 2, false)]
-    [InlineData("   ", 1, 2, false)]
-    [InlineData("\t", 1, 2, false)]
-    [InlineData("\n", 1, 2, false)]
-    public void Constructor_SpecificSchema_SingleFrom_InvalidSchema_ThrowsException(string schema, int from, int to, bool shouldAvoid)
+    [InlineData("", 1, 2)]
+    [InlineData("   ", 1, 2)]
+    [InlineData("\t", 1, 2)]
+    [InlineData("\n", 1, 2)]
+    public void Constructor_SpecificSchema_SingleFrom_InvalidSchema_ThrowsException(string schema, int from, int to)
     {
-        Assert.Throws<ArgumentException>(() => new MigrationAttribute(schema, from, to, shouldAvoid));
+        Assert.Throws<ArgumentException>(() => new MigrationAttribute(schema, from, to));
     }
 
     #endregion
@@ -77,74 +82,51 @@ public class MigrationAttributeSingleFromTests
     #region Constructor: MigrationAttribute(string? fromSchema, int from, string? toSchema, int to, bool shouldAvoid = false)
 
     [Theory]
-    [InlineData(null, 1, null, 2, false, new int[] { 1 }, 2, false, false)]
-    [InlineData(null, 1, null, 2, true, new int[] { 1 }, 2, false, true)]
-    [InlineData("free", 1, "free", 2, false, new int[] { 1 }, 2, false, false)]
-    [InlineData("free", 1, "free", 2, true, new int[] { 1 }, 2, false, true)]
-    [InlineData("free", 10, "paid", 1, false, new int[] { 10 }, 1, false, false)]
-    [InlineData("free", 10, "paid", 1, true, new int[] { 10 }, 1, false, true)]
-    [InlineData("paid", 5, "enterprise", 1, false, new int[] { 5 }, 1, false, false)]
-    [InlineData("paid", 5, "enterprise", 1, true, new int[] { 5 }, 1, false, true)]
-    [InlineData(null, 5, "premium", 1, false, new int[] { 5 }, 1, false, false)]
-    [InlineData("basic", 5, null, 10, false, new int[] { 5 }, 10, false, false)]
-    public void Constructor_CrossSchema_SingleFrom_Success(string? fromSchema, int from, string? toSchema, int to, bool shouldAvoid, int[] expectedFromVersions, int expectedTo, bool expectedIsRange, bool expectedShouldAvoid)
+    [InlineData(null, 1, null, 2, true, new[] { 1 }, null, null)]
+    [InlineData(null, -11, null, 2, false, new[] { -11 }, null, null)]
+    [InlineData("free", 1, "free", 200, null, new[] { 1 }, "free", "free")]
+    [InlineData("free", -101, "free", -200, true, new[] { -101 }, "free", "free")]
+    [InlineData("free", 10, "paid", 1, false, new[] { 10 }, "free", "paid")]
+    [InlineData("free ", 10, " paid", 1, null, new[] { 10 }, "free", "paid")]
+    [InlineData("paid\n", 5, "enterprise", 1, true, new[] { 5 }, "paid", "enterprise")]
+    [InlineData("paid", 5, "enterprise", 1, false, new[] { 5 }, "paid", "enterprise")]
+    [InlineData(null, 5, "premium\n\n", 1, null, new[] { 5 }, null, "premium")]
+    [InlineData("basic", 5, null, 10, true, new[] { 5 }, "basic", null)]
+    public void Constructor_CrossSchema_SingleFrom_Success(string? fromSchema, int from, string? toSchema, int to, bool? shouldAvoid, int[] expectedFromVersions, string? expectedFromSchema, string? expectedToSchema)
     {
-        var attribute = new MigrationAttribute(fromSchema, from, toSchema, to, shouldAvoid);
+        var attribute = shouldAvoid.HasValue
+            ? new MigrationAttribute(fromSchema, from, toSchema, to, shouldAvoid.Value)
+            : new MigrationAttribute(fromSchema, from, toSchema, to);
+        Assert.Equal(expectedFromSchema, attribute.FromSchema);
         Assert.Equal(expectedFromVersions, attribute.FromVersions);
-        Assert.Equal(expectedTo, attribute.ToVersion);
-        Assert.Equal(expectedIsRange, attribute.IsRange);
-        Assert.Equal(expectedShouldAvoid, attribute.ShouldAvoid);
-        Assert.Equal(fromSchema, attribute.FromSchema);
-        Assert.Equal(toSchema, attribute.ToSchema);
+        Assert.False(attribute.IsRange);
+        Assert.Equal(expectedToSchema, attribute.ToSchema);
+        Assert.Equal(to, attribute.ToVersion);
+        if (shouldAvoid.HasValue) Assert.Equal(shouldAvoid.Value, attribute.ShouldAvoid);
+        else Assert.False(attribute.ShouldAvoid);
     }
 
     [Theory]
-    [InlineData("", 1, "paid", 2, false)]
-    [InlineData("   ", 1, "paid", 2, false)]
-    [InlineData("free", 1, "", 2, false)]
-    [InlineData("free", 1, "   ", 2, false)]
-    [InlineData("\t", 1, "paid", 2, false)]
-    [InlineData("free", 1, "\n", 2, false)]
-    public void Constructor_CrossSchema_SingleFrom_InvalidSchema_ThrowsException(string fromSchema, int from, string toSchema, int to, bool shouldAvoid)
+    [InlineData("", 1, "paid", 2)]
+    [InlineData("   ", 1, "paid", 2)]
+    [InlineData("free", 1, "", 2)]
+    [InlineData("free", 1, "   ", 2)]
+    [InlineData("\t", 1, "paid", 2)]
+    [InlineData("free", 1, "\n", 2)]
+    [InlineData("  ", 1, "\n", 2)]
+    public void Constructor_CrossSchema_SingleFrom_InvalidSchema_ThrowsException(string fromSchema, int from, string toSchema, int to)
     {
-        Assert.Throws<ArgumentException>(() => new MigrationAttribute(fromSchema, from, toSchema, to, shouldAvoid));
+        Assert.Throws<ArgumentException>(() => new MigrationAttribute(fromSchema, from, toSchema, to));
     }
 
     [Theory]
     [InlineData("free", 5, "paid", 5, false)]
-    [InlineData("free", 5, "paid", 5, true)]
+    [InlineData("free", 5, "paid ", 5, true)]
     [InlineData(null, 10, null, 10, false)]
-    [InlineData("enterprise", -1, "enterprise", -1, true)]
+    [InlineData("\n\nenterprise", -1, "  enterprise \t\r\n  ", -1, true)]
     public void Constructor_CrossSchema_SingleFrom_SameFromTo_ThrowsException(string? fromSchema, int from, string? toSchema, int to, bool shouldAvoid)
     {
         Assert.Throws<ArgumentException>(() => new MigrationAttribute(fromSchema, from, toSchema, to, shouldAvoid));
-    }
-
-    #endregion
-
-    #region Edge Cases for Single From Migrations
-
-    [Fact]
-    public void Constructor_SingleFrom_FromVersionsLength()
-    {
-        var attribute = new MigrationAttribute(5, 10);
-        Assert.Single(attribute.FromVersions);
-        Assert.Equal(5, attribute.FromVersions[0]);
-    }
-
-    [Fact]
-    public void Constructor_SingleFrom_IsRangeFalse()
-    {
-        var attribute = new MigrationAttribute(5, 10);
-        Assert.False(attribute.IsRange);
-    }
-
-    [Fact]
-    public void Constructor_SingleFrom_NegativeVersions()
-    {
-        var attribute = new MigrationAttribute(-100, -50);
-        Assert.Equal(new int[] { -100 }, attribute.FromVersions);
-        Assert.Equal(-50, attribute.ToVersion);
     }
 
     #endregion
